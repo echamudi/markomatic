c3e9a1062a1552dfcf940b08a90b6c4aea47089e
Ezzat Chamudi <chamudi.git@outlook.com>
2019-11-06 13:40:41 +0300

    Add CLI tool

---
+++ cli.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+// markomatic ./test/fixture/basic-variable/source/markomatic.yaml
+// or
+// cd ./test/fixture/basic-variable/source
+// markomatic ./markomatic.yaml
+
+
+const markomatic = require('.');
+const path = require('path');
+
+console.log(process.argv);
+console.log(markomatic(path.join(process.cwd(), process.argv[2])));
