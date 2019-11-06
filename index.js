74fe19ff766a90af79f99d79dfb427dcaed0040d
Ezzat Chamudi <chamudi.git@outlook.com>
2019-11-06 11:15:38 +0300

    Remove try and return result

--- index.js
+++ index.js
@@ -6,19 +6,14 @@ const path = require('path');
 module.exports = function (yamlPath) {
     const yamlDir = path.dirname(yamlPath);
 
-    try {
-        const doc = yaml.safeLoad(fs.readFileSync(yamlPath, 'utf8'));
+    const doc = yaml.safeLoad(fs.readFileSync(yamlPath, 'utf8'));
 
-        const templatePath = path.join(yamlDir, doc.markomatic.template);
-        const outputPath   = path.join(yamlDir, doc.markomatic.output);
+    const templatePath = path.join(yamlDir, doc.markomatic.template);
+    const outputPath   = path.join(yamlDir, doc.markomatic.output);
 
-        const templateText = fs.readFileSync(templatePath, 'utf8');
+    const templateText = fs.readFileSync(templatePath, 'utf8');
 
-        let res = nunjucks.renderString(templateText, doc.markomatic.variables);
+    const res = nunjucks.renderString(templateText, doc.markomatic.variables);
 
-        console.log(doc);
-        console.log(res);
-    } catch (e) {
-        console.log(e);
-    }
+    return res
 }
