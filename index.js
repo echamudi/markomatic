175b071fab3c494c5f3124cffffaf67a783f4460
Ezzat Chamudi <chamudi.git@outlook.com>
2019-11-06 21:55:48 +0300

    Update index

--- index.js
+++ index.js
@@ -8,14 +8,28 @@ module.exports = function (yamlFilePath) {
 
     const yamlObject = yaml.safeLoad(fs.readFileSync(yamlFilePath, 'utf8'));
 
-    const templateFilePath = path.join(yamlDir, yamlObject.markomatic.template);
-    const templateDir = path.dirname(templateFilePath);
-    const outputFilePath   = path.join(yamlDir, yamlObject.markomatic.output);
+    const inputFilePath = path.join(yamlDir, yamlObject.markomatic.input);
+    const inputFileName = path.parse(inputFilePath).base;
+    const inputDir = path.dirname(inputFilePath);
+    const outputFilePath = path.join(yamlDir, yamlObject.markomatic.output);
     const outputDir = path.dirname(outputFilePath);
+    const templateDirs = [];
 
-    const templateText = fs.readFileSync(templateFilePath, 'utf8');
+    yamlObject.markomatic.templateDirs.forEach((templateDir) => {
+        templateDirs.push(path.join(yamlDir, templateDir));
+    });
 
-    const renderedText = nunjucks.renderString(templateText, yamlObject.markomatic.variables);
+    nunjucks.configure(
+        // Include lookup order
+        [
+            inputDir,       // 1. Same folder as input file
+            yamlDir,        // 2. Same folder as yaml file
+            ...templateDirs,    // 3. templateDirs written in yaml file
+        ],
+        {
+            autoescape: false // Don't use HTML character
+        });
+    const renderedText = nunjucks.render(inputFileName, yamlObject.markomatic.variables);
 
     fs.mkdirSync(outputDir, { recursive: true });
     fs.writeFileSync(outputFilePath, renderedText);
