2dfe5af0157911f6a4c3f5211164c58e3b203f61
Ezzat Chamudi <chamudi.git@outlook.com>
2019-11-11 14:54:05 +0300

    Update

--- index.js
+++ index.js
@@ -4,29 +4,46 @@ const nunjucks = require('nunjucks');
 const path = require('path');
 const os = require('os');
 
-module.exports = function (yamlFilePath) {
-    const yamlDir = path.dirname(yamlFilePath);
+const EOL_MODE_CRLF = 0;
+const EOL_MODE_LF = 1;
 
+module.exports = function (yamlFilePath) {
+
+    // Get config yaml as object
+    const yamlDir = path.dirname(yamlFilePath);
     const yamlObject = yaml.safeLoad(fs.readFileSync(yamlFilePath, 'utf8'));
 
     // Check yaml validity
     if (yamlObject.markomatic === undefined) throw new Error('The provided yaml file doesn\'t have markomatic property.');
 
+    // Get in put and output data
     const inputFilePath = path.join(yamlDir, yamlObject.markomatic.input);
     const inputFileName = path.parse(inputFilePath).base;
     const inputDir = path.dirname(inputFilePath);
     const inputText = fs.readFileSync(inputFilePath, 'utf8');
 
-    console.log(inputText.split('\r\n').length - 1);
-
     const outputFilePath = path.join(yamlDir, yamlObject.markomatic.output);
     const outputDir = path.dirname(outputFilePath);
+
+    // Decide CRLF and LF mode by how much CRLF or LF occur in the input file
+    const crlfCount = inputText.split('\r\n').length - 1;
+    const lfCount = inputText.split('\n').length - crlfCount;
+
+    let eolMode;
+    if (lfCount < crlfCount) {
+        eolMode = EOL_MODE_CRLF
+    } else {
+        eolMode = EOL_MODE_LF;
+    };
+
+    // Collect template directories from the configuration yaml
     const templateDirs = [];
 
     yamlObject.markomatic.templateDirs.forEach((templateDir) => {
         templateDirs.push(path.join(yamlDir, templateDir));
     });
 
+    // Configure nunjucks
     nunjucks.configure(
         // Include lookup order
         [
@@ -37,8 +54,19 @@ module.exports = function (yamlFilePath) {
         {
             autoescape: false // Don't use HTML character
         });
-    const renderedText = nunjucks.render(inputFileName, yamlObject.markomatic.variables);
 
+    // Run nunjucks
+    let renderedText = nunjucks.render(inputFileName, yamlObject.markomatic.variables);
+
+    // Fix  CRLF or LF
+    if (eolMode === EOL_MODE_CRLF) {
+        renderedText = renderedText.replace(/\r\n/g, "\n");
+        renderedText = renderedText.replace(/\n/g, "\r\n");
+    } else {
+        renderedText = renderedText.replace(/\r\n/g, "\n");
+    }
+
+    // Write file
     fs.mkdirSync(outputDir, { recursive: true });
     fs.writeFileSync(outputFilePath, renderedText);
 
